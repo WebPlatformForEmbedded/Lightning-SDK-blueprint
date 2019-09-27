1453bb01b341bb315b44a4796100cc250566d886
Grzegorz Zachar <g.zachar@metrological.com>
2019-09-27 12:37:13 +0200

    Example localization

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-export default class App extends ux.App {
+ux.locale.setLanguage('en');
 
+export default class App extends ux.App {
     static getFonts() {
         return [
         ]
     }
 
     static _template() {
-        return {
-        };
-    }
-
+      return {
+        Text: {
+          w: 700,
+          text: {
+            text: ux.locale.tr.hello2.format("A", 2),
+            fontSize: 32,
+        }
+      }
+    };
+  }
 }
